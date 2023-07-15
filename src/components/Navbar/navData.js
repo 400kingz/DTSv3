c7e0317b67bfc26fab7526fd797ff7b63bb8fdd3
400kingz <flurpro@gmail.com>
2023-07-14 18:09:13 -0700

    yes

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -8,10 +8,6 @@ export const navLinks = [
     pathname: '/#details',
   },
   {
-    label: 'Articles',
-    pathname: '/articles',
-  },
-  {
     label: 'Contact',
     pathname: '/contact',
   },
@@ -20,17 +16,7 @@ export const navLinks = [
 export const socialLinks = [
   {
     label: 'Twitter',
-    url: 'https://twitter.com/hamishMW',
+    url: 'https://daretosurpassv3.vercel.app',
     icon: 'twitter',
-  },
-  {
-    label: 'Figma',
-    url: 'https://www.figma.com/@Hamish',
-    icon: 'figma',
-  },
-  {
-    label: 'Github',
-    url: 'https://github.com/HamishMW',
-    icon: 'github',
   },
 ];
